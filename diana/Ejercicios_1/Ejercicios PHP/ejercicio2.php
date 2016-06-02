81e88555e417f573a6d282a9bb2cecdabf66704a
Diana Carrizales <diana.carrizales.dja@gmail.com>
2016-06-02 08:05:22 -0500

    Ejercicios_1
    Ejercicios de PHP y de HTML/CSS

---
+++ diana/Ejercicios_1/Ejercicios PHP/ejercicio2.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>Ejercicio2 de PHP</title>
+	</head>
+	<body>
+		<?php phpinfo();?>
+	</body>
+</html>
---
+++ diana/Ejercicios_1/Ejercicios PHP/index.php
@@ -0,0 +1,8 @@
+<html>
+<head></head>
+<body>
+<?php
+  echo "Hola Mundo";
+?>
+</body>
+</html>
